94000bd0a2465eea25cf98b21aa9062b5652d6c4
Ford Henley <fordp86@gmail.com>
2023-07-19 14:56:18 -0500

    Setup Pagniation and File Uploads
    I setup Pagniation and started file upload settings

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -2,7 +2,7 @@
     @include('partials._hero')
     @include('partials._search')
     <div
-    class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+    class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-2 mx-8"
     >
     @unless(count($listings) == 0)
     @foreach($listings as $listing)
@@ -13,5 +13,8 @@
         <p>No listings found</p>
     @endunless
     
-    </div>    
+    </div>
+    <div class="mt-6 p-4">
+        {{$listings->links()}}
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,13 @@ use App\Models\Listing;
 // All listings
 Route::get('/', [ListingController::class,'index']);
 
+// Show Cream Form
+Route::get('/listings/create', [ListingController::class,'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
